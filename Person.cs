3b9f0bba2ed5283689ac04070ab3c5b9fdd7385a
Christoper <c.blake@hotmail.se>
2021-06-23 08:13:32 +0200

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,24 @@
+﻿namespace OOP2
+{
+    internal class Person
+    {
+        private int Age;
+        private string FName;
+        private string LName;
+        private double Height;
+        private double Weight;
+
+        public Person(int age, string fName, string lName, double height, double weight)
+        {
+            this.Age = age;
+            this.FName = fName;
+            this.LName = lName;
+            this.Height = height;
+            this.Weight = weight;
+        }
+        public override string ToString()
+        {
+            return $"Name: {FName} LName: {LName}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Person person = new Person(1,"Hello","There",125.5, 128.5);
+
+            Console.WriteLine(person.ToString());
+        }
+    }
+}
